2da693009b45357a87c32e361dfb58570e6486d5
PetraElgemyr <petra.elgemyr@gmail.com>
2025-04-24 09:29:28 +0200

    clean project and add usercard

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { IUser } from "./interfaces/IUser";
+import UserCard from "./components/UserCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<IUser[]>([
+    {
+      id: 1,
+      firstName: "Cecilia",
+      lastName: "Sporrong",
+      email: "blabla@gmail.com",
+    },
+    {
+      id: 2,
+      firstName: "Arvid",
+      lastName: "Berlin",
+      email: "hejhej@domain.com",
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {users.map((user) => (
+        <UserCard key={user.id} user={user}></UserCard>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,19 @@
+import { IUser } from "../interfaces/IUser";
+
+interface IUserCardProps {
+  user: IUser;
+}
+
+const UserCard = ({ user }: IUserCardProps) => {
+  return (
+    <div>
+      <h1>User {user.id}</h1>
+      <p>
+        {user.firstName} {user.lastName}
+      </p>
+      <p>{user.email}</p>
+    </div>
+  );
+};
+
+export default UserCard;
